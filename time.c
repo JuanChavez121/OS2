9242e7c8468396bc6cdc8bd9c6a8fbf123d35c5c
JuanChavez121 <jchavez9198@gmail.com>
2025-10-21 19:48:01 -0400

    updatedFix

--- time.c
+++ time.c
@@ -2,6 +2,7 @@
 
 double elapsed_time(struct timeval* start_time, struct timeval* end_time){
     //TODO: return the difference between end_time and start_time.
-    
-    return 0.0;
+    double sec = (double)((*end_time).tv_sec - (*start_time).tv_sec);
+    double microsec = (double)(*end_time).tv_usec - (*start_time).tv_usec/1000000.0;
+    return sec + microsec;
 }
